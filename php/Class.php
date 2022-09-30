53397703405004dc82fadbb94789b415499478b0
Wasim A Pinjari <axmidx101@gmail.com>
2022-09-30 16:26:27 +0530

    Update Class.php

--- php/Class.php
+++ php/Class.php
@@ -1,15 +1,22 @@
 <?php
 
 class Car {
-  public $name, $price;
+
+  public $name = "BMW",
+  $price = "$75,000";
+
   function car(){
-    echo $this->name . " car price is: " . $this->price;
+    echo $this->name . " car price is: " . $this->price . "<br>";
   }
+
 }
+
+$car2 = new Car();
+$car2->car(); // Output: BMW car price is: $75,000
 
 $car1 = new Car();
 $car1->name = "Audi";
 $car1->price = "$50,000";
-$car1->car();
+$car1->car(); // Output: Audi car price is: $50,000
 
-?> // Output: Audi car price is: $50,000
+?>
