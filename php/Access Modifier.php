d55e0268e597a77c243eaa421e5207ac6bdcd3c8
Wasim A Pinjari <axmidx101@gmail.com>
2022-09-30 22:02:36 +0530

    Update Access Modifier.php

--- php/Access Modifier.php
+++ php/Access Modifier.php
@@ -6,7 +6,7 @@ Class Money {
   protected $creditCard;
   private $debitCard;
 
-  public function __construct($cash = 3000, $creditCard = 10000, $debitCard = 50000) {
+  public function __construct( $cash = 3000, $creditCard = 10000, $debitCard = 50000 ) {
     $this->cash = $cash;
     $this->creditCard = $creditCard;
     $this->debitCard = $debitCard;
@@ -42,17 +42,21 @@ Class Loan extends Money {
 
 }
 
+// You can get output by commenting and uncommenting certain code lines:
 
-$money1 = new Money(4000, 15000, 55000);
+$money1 = new Money( 4000, 15000, 55000 );
 
 $money1->moneyInCash(); // Output: Total Money in Cash: $4000 (public: $cash variable and moneyInCash() function)
 $money1->moneyInCreditCard(); // Output: Error (protected: $creditCard variable and moneyInCreditCard() function)
 $money1->moneyInDebitCard(); // Output: Error (private $debitCard variable and moneyInDebitCard() function)
 
-$money2 = new Loan(4000, 15000, 55000);
+$money2 = new Loan( 4000, 15000, 55000 );
 
 $money2->moneyInCash(); // Output: Total Money in Cash: $4000 (public: $cash variable / public: moneyInCash() function)
 $money2->moneyInCreditCard(); // Output: Total Money in Credit Card: $15000 (protected: $creditCard / public: moneyInCreditCard() function)
 $money2->moneyInDebitCard(); // Output: Error (private: $debitCard / public: moneyInDebitCard() function)
+
+$money2->debitCard = 30000; // Redefining undefined private $debitCard variable
+$money2->moneyInDebitCard(); // Output: Total Money in Debit Card: $30000
 
 ?>
